6a612f645e2219d5e3d1e5fb9b8a0ac94e7ce369
makhdumz <86830353+makhdumz@users.noreply.github.com>
2025-09-25 18:19:08 +0500

    terrrafrom

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Use existing Resource Group
+data "azurerm_resource_group" "rg" {
+  name = "low"
+}
+
+# App Service Plan (Linux Free F1)
+resource "azurerm_service_plan" "plan" {
+  name                = "portfolio-plan"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+# Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = "portfolioregistrytf020qrb"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Linux Web App running Docker container
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "portfolio-demo"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  # App settings point to Docker image in ACR
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL"      = "https://${azurerm_container_registry.acr.login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+}
